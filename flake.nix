20d562a1b26e1c0380a08ac8aec1a2abef6850f5
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-16 19:06:54 +0000

    feat(flake): Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    neovim-config.url = "github:evictedcucumber/neovim-config/main";
+    neovim-config.flake = false;
+  };
+
+  outputs = {nixpkgs, ...} @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config.allowUnfree = true;
+    };
+  in {
+    nixosConfigurations."nixos-wsl" = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [./system/nixos-wsl/configuration.nix];
+      specialArgs = {
+        nixos-wsl = inputs.nixos-wsl;
+        username = "ethan";
+        hostname = "nixos-wsl";
+      };
+    };
+    homeConfigurations."ethan" = inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [./home/ethan/home.nix];
+      extraSpecialArgs = {neovim-config = inputs.neovim-config;};
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [nixd alejandra zsh];
+
+      shellHook = ''
+        if [[ "$(basename "$0")" != "zsh" ]]; then
+            exec ${pkgs.zsh}/bin/zsh -l
+        fi
+      '';
+    };
+  };
+}
