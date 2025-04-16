ebcc0c5bc114f30071d6ba27d9cf89aa225ecae4
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-16 19:05:00 +0000

    feat(mod_cli): Added `neovim`

---
+++ modules/cli/neovim.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  config,
+  pkgs,
+  neovim-config,
+  ...
+}: {
+  options = {me.cli.neovim.enable = lib.mkEnableOption "Enable Neovim";};
+
+  config = lib.mkIf config.me.cli.neovim.enable {
+    home.packages = with pkgs; [
+      gnumake
+      gcc
+      luarocks
+      python3
+      python3Packages.pip
+      python3Packages.virtualenv
+      lua5_1
+      lua51Packages.jsregexp
+      lua51Packages.sqlite
+      unzip
+      wget
+      nodejs
+      cargo
+      rustc
+      sqlite
+      tree-sitter
+      neovim-unwrapped
+    ];
+
+    home.file = {
+      ".config/nvim/init.lua".source = "${neovim-config}/init.lua";
+      ".config/nvim/after".source = "${neovim-config}/after";
+      ".config/nvim/lua".source = "${neovim-config}/lua";
+    };
+  };
+}
