3582e82a28ff67410e30bfe5ff51fb0be5dcf7f9
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-16 19:04:37 +0000

    feat(mod_cli): Added `git`

---
+++ modules/cli/git.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  username,
+  ...
+}: {
+  options = {me.cli.git.enable = lib.mkEnableOption "Enable GIT";};
+
+  config = lib.mkIf config.me.cli.git.enable {
+    programs.git.enable = true;
+    programs.git.userName = "Ethan";
+    programs.git.userEmail = "95688781+evictedcucumber@users.noreply.github.com";
+    programs.git.extraConfig = {
+      user.signingkey = "8E29907A4CA30E30";
+      init.defaultbranch = "main";
+      pull.rebase = true;
+      rebase.updaterefs = true;
+      commit.gpgsign = true;
+      tag.gpgSign = true;
+    };
+  };
+}
