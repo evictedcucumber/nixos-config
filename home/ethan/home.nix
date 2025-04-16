bffff4430ea6989c0aceeba2959032586e9db6c7
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-16 19:02:51 +0000

    feat(home): Added 'ethan' home.nix

---
+++ home/ethan/home.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  # pkgs,
+  ...
+}: {
+  home.username = "ethan";
+  home.homeDirectory = "/home/${config.home.username}";
+  home.stateVersion = "24.11";
+
+  imports = [../../modules/gui ../../modules/cli];
+
+  me.gui.enable = true;
+  me.cli.enable = true;
+
+  me.cli.shells.zsh.enable = true;
+
+  xdg = {
+    enable = true;
+    configHome = "${config.home.homeDirectory}/.config";
+    dataHome = "${config.home.homeDirectory}/.local/share";
+    stateHome = "${config.home.homeDirectory}/.local/state";
+    cacheHome = "${config.home.homeDirectory}/.local/cache";
+  };
+
+  # home.packages = with pkgs; [ ];
+
+  home.file = {};
+
+  home.sessionVariables = {};
+
+  programs.home-manager.enable = true;
+}
