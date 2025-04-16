54a59a623fbfeb7d29a547574509a11614871ea9
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-16 19:04:28 +0000

    feat(mod_cli): Added default cli module

---
+++ modules/cli/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  imports = [
+    ./shells
+    ./yazi.nix
+    ./tmux.nix
+    ./neovim.nix
+    ./git.nix
+    ./lazygit.nix
+    ./bottom.nix
+  ];
+
+  options = {me.cli.enable = lib.mkEnableOption "Enable CLI Tools";};
+
+  config = lib.mkIf config.me.cli.enable {
+    me.cli.shells.enable = true;
+    me.cli.yazi.enable = true;
+    me.cli.tmux.enable = true;
+    me.cli.neovim.enable = true;
+    me.cli.git.enable = true;
+    me.cli.lazygit.enable = true;
+    me.cli.bottom.enable = true;
+  };
+}
