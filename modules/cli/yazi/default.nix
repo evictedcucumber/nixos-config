2b0f440787cd6df6e0b3b79719f173c12643464d
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-07-06 17:26:14 +0000

    fix(yazi): changed yazi module to use new modules

--- modules/cli/yazi/default.nix
+++ modules/cli/yazi/default.nix
@@ -4,9 +4,26 @@
   pkgs,
   ...
 }: {
-  options = {me.cli.yazi.enable = lib.mkEnableOption "Enable Yazi";};
+  imports = [
+    ../fd.nix
+    ../ripgrep.nix
+    ../fzf.nix
+    ../zoxide.nix
+  ];
+
+  options = {
+    me.cli.yazi = {
+      enable = lib.mkEnableOption "Enable Yazi";
+      enableFish = lib.mkEnableOption "Enable Yazi Fish Integration";
+    };
+  };
 
   config = lib.mkIf config.me.cli.yazi.enable {
+    me.cli.fd.enable = true;
+    me.cli.ripgrep.enable = true;
+    me.cli.fzf.enable = true;
+    me.cli.zoxide.enable = true;
+
     home.packages = with pkgs; [
       file
       nerd-fonts.jetbrains-mono
@@ -14,17 +31,16 @@
       p7zip
       jq
       poppler
-      fd
-      ripgrep
-      fzf
-      zoxide
       resvg
       imagemagick
     ];
 
-    programs.yazi.enable = true;
-    programs.yazi.package = pkgs.yazi;
-    programs.yazi.enableZshIntegration = true;
+    programs.yazi = {
+      enable = true;
+      package = pkgs.yazi;
+      enableFishIntegration = config.me.cli.yazi.enableFish;
+    };
+
     home.file.".config/yazi/yazi.toml".source = ./yazi.toml;
     home.file.".config/yazi/theme.toml".source = ./theme.toml;
     home.file.".config/yazi/keymap.toml".source = ./keymap.toml;
