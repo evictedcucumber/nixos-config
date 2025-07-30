784ae8177d4205d6d835d96fe8f20e2eac83c88f
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-07-30 15:46:52 +0000

    feat(nixos-wsl): added Google Drive sync to improve performance

--- system/nixos-wsl/configuration.nix
+++ system/nixos-wsl/configuration.nix
@@ -2,7 +2,19 @@
   pkgs,
   nixos-wsl,
   ...
-}: {
+}: let
+  unisonWatchScript = pkgs.writeShellScript "unison-watch" ''
+    #/usr/bin/env bash
+
+    while true; do
+      ${pkgs.inotify-tools}/bin/inotifywait -r -e modify,create,delete,move "/mnt/mydrive/[97] Obsidian Vault" /home/ethan/myvault
+      ${pkgs.unison}/bin/unison \
+        -root "/mnt/mydrive/[97] Obsidian Vault" \
+        -root "/home/ethan/myvault" \
+        -batch -prefer=newer -fat
+    done
+  '';
+in {
   imports = [
     ../../system
     nixos-wsl.nixosModules.wsl
@@ -17,6 +29,7 @@
     extraGroups = ["wheel"];
     shell = pkgs.fish;
   };
+  nix.settings.trusted-users = ["ethan"];
 
   fileSystems."/mnt/mydrive" = {
     device = "G:/My\\040Drive";
@@ -29,5 +42,15 @@
       "gid=100"
       "uid=1000"
     ];
+  };
+
+  environment.systemPackages = with pkgs; [unison inotify-tools];
+  systemd.user.services.unison-watch = {
+    description = "Run unison-watch.sh to sync obsidian vault";
+    serviceConfig = {
+      ExecStart = unisonWatchScript;
+      Restart = "always";
+    };
+    wantedBy = ["default.target"];
   };
 }
