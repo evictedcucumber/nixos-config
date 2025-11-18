df08b168cd3015880c3fc52c8aa5e10268c7922e
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-11-18 22:22:01 +0000

    feat(cli/bottom): updated theme version

--- modules/cli/bottom.nix
+++ modules/cli/bottom.nix
@@ -1,15 +1,20 @@
 {
   lib,
   config,
+  pkgs,
   ...
-}: {
+}: let
+  catpuccin_theme = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "bottom";
+    rev = "main";
+    sha256 = "sha256-dfukdk70ug1lRGADKBnvMhkl+3tsY7F+UAwTS2Qyapk=";
+  };
+in {
   options = {me.cli.bottom.enable = lib.mkEnableOption "Enable bottom";};
 
   config = lib.mkIf config.me.cli.bottom.enable {
     programs.bottom.enable = true;
-    home.file.".config/bottom/bottom.toml".source = builtins.fetchurl {
-      url = "https://github.com/catppuccin/bottom/raw/main/themes/mocha.toml";
-      sha256 = "1bvgr6xhhk9l34ndypia4vjgdmaxlz4qnka99ivr3krgx8iwf60r";
-    };
+    home.file.".config/bottom/bottom.toml".source = "${catpuccin_theme}/themes/mocha.toml";
   };
 }
