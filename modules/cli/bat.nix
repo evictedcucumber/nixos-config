06ee018aaab6f1bce218be19b6e4256b05bbf988
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-11-18 22:21:43 +0000

    feat(cli/bat): added more extras and theme updates

--- modules/cli/bat.nix
+++ modules/cli/bat.nix
@@ -9,21 +9,33 @@
   };
 
   config = lib.mkIf config.me.cli.bat.enable {
-    home.packages = with pkgs; [bat-extras.batman];
+    home.packages = with pkgs.bat-extras; [
+      batman
+      batdiff
+      batgrep
+      batpipe
+      batdiff
+      batwatch
+    ];
 
-    home.sessionVariables = {
-      BAT_THEME = "catppuccin";
-    };
+    # home.sessionVariables = {
+    #   BAT_THEME = "catppuccin";
+    # };
 
     programs.bat = {
       enable = true;
+      config = {
+        theme = "catppuccin";
+      };
+      syntaxes = {};
       themes = {
         catppuccin = {
           src = pkgs.fetchFromGitHub {
             owner = "catppuccin";
             repo = "bat";
             rev = "main";
-            sha256 = "sha256-6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+            # sha256 = lib.fakeSha256;
+            sha256 = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
           };
           file = "themes/Catppuccin Mocha.tmTheme";
         };
