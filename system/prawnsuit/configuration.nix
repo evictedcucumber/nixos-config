34baa1f45a41d75f4b619c523c2db5dcca5c0b9c
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-12-30 20:03:30 +0200

    feat(system/prawnsuit): fixed display

--- system/prawnsuit/configuration.nix
+++ system/prawnsuit/configuration.nix
@@ -18,7 +18,10 @@
 
   networking.networkmanager.enable = true;
 
-  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
   services.desktopManager.gnome.enable = true;
   environment.gnome.excludePackages = with pkgs; [
     gnome-contacts
@@ -44,6 +47,11 @@
     yelp
     papers
   ];
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
 
   services.xserver = {
     enable = true;
@@ -68,11 +76,44 @@
     jack.enable = true;
   };
 
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      swtpm.enable = true;
+    };
+  };
+  virtualisation.spiceUSBRedirection.enable = true;
+  networking.firewall.trustedInterfaces = ["virbr0"];
+  programs.virt-manager.enable = true;
+
   programs.fish.enable = true;
   users.users."${username}" = {
     isNormalUser = true;
-    extraGroups = ["networkmanager" "wheel"];
+    extraGroups = ["networkmanager" "wheel" "libvirtd" "kvm"];
     shell = pkgs.fish;
   };
   nix.settings.trusted-users = ["${username}"];
+
+  services.avahi.enable = false;
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      TLP_AUTO_SWITCH = 1;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wwan";
+      DEVICES_TO_ENABLE_ON_STARTUP = "wifi";
+      PCIE_ASPM_ON_AC = "performance";
+      PCIE_ASPM_ON_BAT = "powersave";
+    };
+  };
+
+  services.fwupd.enable = true;
+
+  hardware.cpu.intel.updateMicrocode = true;
 }
