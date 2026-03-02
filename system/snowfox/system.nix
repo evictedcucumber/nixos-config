cb23d86728bf3a06f4a1acb2749da4676933af45
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-03-02 13:30:48 +0200

    feat: add obsidian-headless sync

--- system/snowfox/system.nix
+++ system/snowfox/system.nix
@@ -1,4 +1,8 @@
-{username, ...}: {
+{
+  config,
+  username,
+  ...
+}: {
   imports = [../../system];
 
   networking.hostName = "snowfox";
@@ -6,6 +10,18 @@
   wsl.enable = true;
   wsl.wslConf.interop.appendWindowsPath = false;
   wsl.defaultUser = username;
+
+  systemd.user.services."vault-sync" = {
+    description = "Continuous sync obsidian vault.";
+    after = ["default.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "/bin/bash -lc '/home/${username}/.npm-global/bin/ob sync --path /home/${username}/myvault --continuous'";
+      Restart = "always";
+      RestartSec = 5;
+    };
+    wantedBy = ["default.target"];
+  };
 
   home-manager.users.${username} = {
     imports = [
@@ -15,8 +31,13 @@
 
     me.cli.git.signingkey = "A2FD5AF74494FD44";
 
-    home.sessionVariables = {
-      LEDGER_FILE = "/mnt/c/Users/ethan/Documents/My Obsidian Vault/97 - Finance/main.journal";
+    home.sessionVariables = let
+      configHome = config.home-manager.users.${username}.home.sessionVariables;
+    in {
+      LEDGERS_DIR = "/home/${username}/myvault/97 - Finance";
+      LEDGER_FILE = "${configHome.LEDGERS_DIR}/main.journal";
+      CURRENT_LEDGER_FILE = "${configHome.LEDGERS_DIR}/current.journal";
+      YEAR_LEDGER_FILE = "${configHome.LEDGERS_DIR}/2026.journal";
     };
   };
 }
