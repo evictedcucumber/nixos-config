722ca0358de930359b7cfe397fde0598d232865e
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-03-03 15:09:50 +0200

    fix: fixed bat config clashes

--- home/modules/cli/bat.nix
+++ home/modules/cli/bat.nix
@@ -3,18 +3,21 @@
   pkgs,
   ...
 }: {
-  programs.bat = {enable = true;};
-
-  home.packages = with pkgs.bat-extras; [
-    batdiff
-    batdiff
-    batgrep
-    batman
-    batpipe
-    batwatch
-  ];
+  home.packages = with pkgs;
+    [bat]
+    ++ (with pkgs.bat-extras; [
+      batdiff
+      batdiff
+      batgrep
+      batman
+      batpipe
+      batwatch
+    ]);
 
   home.sessionVariables.BAT_THEME = "catppuccin";
 
-  xdg.configFile."bat".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repos/nixos-config/config/bat";
+  xdg.configFile."bat" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/repos/nixos-config/config/bat";
+    recursive = true;
+  };
 }
