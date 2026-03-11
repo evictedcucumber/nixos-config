bbbb7d33ab22cab4e1b7fc121f504a07cb2163e8
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-03-11 22:40:55 +0200

    feat: add snapper btrfs snapshotting

--- system/seamoth/system.nix
+++ system/seamoth/system.nix
@@ -65,6 +65,41 @@
     upower.enable = true;
     power-profiles-daemon.enable = true;
     fwupd.enable = true;
+    snapper = {
+      configs = {
+        root = {
+          SUBVOLUME = "/";
+          TIMELINE_CREATE = true;
+          TIMELINE_CLEANUP = true;
+          TIMELINE_LIMIT_HOURLY = "0";
+          TIMELINE_LIMIT_DAILY = "0";
+          TIMELINE_LIMIT_WEEKLY = "7";
+          TIMELINE_LIMIT_MONTHLY = "0";
+          TIMELINE_LIMIT_YEARLY = "0";
+        };
+        home = {
+          SUBVOLUME = "/home";
+          TIMELINE_CREATE = true;
+          TIMELINE_CLEANUP = true;
+          TIMELINE_LIMIT_HOURLY = "0";
+          TIMELINE_LIMIT_DAILY = "7";
+          TIMELINE_LIMIT_WEEKLY = "7";
+          TIMELINE_LIMIT_MONTHLY = "0";
+          TIMELINE_LIMIT_YEARLY = "0";
+        };
+        nix = {
+          SUBVOLUME = "/nix";
+          TIMELINE_CREATE = true;
+          TIMELINE_CLEANUP = true;
+          TIMELINE_LIMIT_HOURLY = "0";
+          TIMELINE_LIMIT_DAILY = "0";
+          TIMELINE_LIMIT_WEEKLY = "7";
+          TIMELINE_LIMIT_MONTHLY = "0";
+          TIMELINE_LIMIT_YEARLY = "0";
+        };
+      };
+      snapshotRootOnBoot = true;
+    };
   };
 
   hardware.bluetooth.enable = true;
