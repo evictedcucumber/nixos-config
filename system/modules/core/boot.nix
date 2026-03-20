e0a6cf585a84362025da1d5276470b44849ff68c
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-03-20 17:42:42 +0200

    feat: upgrade to latest kernel version

--- system/modules/core/boot.nix
+++ system/modules/core/boot.nix
@@ -7,6 +7,6 @@
   options.me.system.core.boot.enable = lib.mkEnableOption "Enable Boot Options";
 
   config = lib.mkIf config.me.system.core.boot.enable {
-    boot.kernelPackages = pkgs.linuxPackages_6_18;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
   };
 }
