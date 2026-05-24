254a3cd7f985f72400a41309912a6efc95216720
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-05-24 14:30:30 +0200

    feat: change noctalia to use file specific links not directory

--- modules/home/modules/wms/noctalia.nix
+++ modules/home/modules/wms/noctalia.nix
@@ -4,6 +4,7 @@
 
     programs.noctalia-shell.enable = true;
 
-    xdg.configFile."noctalia".source = ../../../../config/noctalia;
+    xdg.configFile."noctalia/settings.json".source = ../../../../config/noctalia/settings.json;
+    xdg.configFile."noctalia/plugins.json".source = ../../../../config/noctalia/plugins.json;
   };
 }
