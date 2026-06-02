986cd203cfa16c7a08f9f78453511133b0b30086
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-06-02 18:22:55 +0200

    fix: vscode package keyring error

--- modules/home/modules/editors/vscode.nix
+++ modules/home/modules/editors/vscode.nix
@@ -1,5 +1,20 @@
 {...}: {
-  flake.homeModules.vscode = {...}: {
-    programs.vscode.enable = true;
+  flake.homeModules.vscode = {pkgs, ...}: let
+    vscodeWrapped = pkgs.symlinkJoin {
+      name = "vscode-wrapped";
+      paths = [pkgs.vscode];
+
+      buildInputs = [pkgs.makeWrapper];
+
+      postBuild = ''
+        wrapProgram $out/bin/code \
+          --set XDG_CURRENT_DESKTOP GNOME
+      '';
+    };
+  in {
+    programs.vscode = {
+      enable = true;
+      package = vscodeWrapped;
+    };
   };
 }
