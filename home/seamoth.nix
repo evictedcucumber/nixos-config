788324d6311e164cbd4f8099bdc973056e755525
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-08-26 17:56:36 +0200

    feat: move common modules into default

--- home/seamoth.nix
+++ home/seamoth.nix
@@ -6,88 +6,28 @@
   imports = [./default.nix];
 
   # :: ME {
+  # Delta on top of the common profile in ./default.nix.
   me = {
-    archive.zip.enable = true;
     browsers = {
       brave.enable = true;
       helium.enable = true;
     };
-    databases.sqlite.enable = true;
-    editors = {
-      neovim.enable = true;
-      vscode.enable = true;
-    };
-    files = {
-      nautilus.enable = true;
-      yazi.enable = true;
-    };
+    editors.vscode.enable = true;
+    files.nautilus.enable = true;
     # games.modrinth.enable = true;
     media = {
       flameshot.enable = true;
       mpv.enable = true;
       spotify.enable = true;
     };
-    multiplexers.tmux.enable = true;
-    navigation = {
-      fzf.enable = true;
-      tv.enable = true;
-      zoxide.enable = true;
-    };
     notes.obsidian.enable = true;
-    programming = {
-      gcc.enable = true;
-      ghostscript.enable = true;
-      java.enable = true;
-      javascript.enable = true;
-      latex.enable = true;
-      lua.enable = true;
-      mermaid.enable = true;
-      python.enable = true;
-      rust.enable = true;
-      treesitter.enable = true;
-    };
-    searchers = {
-      fd.enable = true;
-      jq.enable = true;
-      ripgrep.enable = true;
-    };
-    shells.fish = {
-      enable = true;
-      integrations.enable = true;
-    };
     terminals.ghostty.enable = true;
     tools = {
-      bat.enable = true;
-      direnv.enable = true;
-      eza.enable = true;
-      git = {
-        enable = true;
-        settings.signingKey = "CB029F0E386B37C7";
-      };
+      git.settings.signingKey = "CB029F0E386B37C7";
       hledger = {
         enable = true;
         ledgersDir = "${config.home.homeDirectory}/Documents/My Obsidian Vault/97 - Finance";
       };
-      lazygit.enable = true;
-      make.enable = true;
-      nh = {
-        enable = true;
-        flake = "${config.home.homeDirectory}/repos/nixos-config";
-      };
-      ssh = {
-        enable = true;
-        hasGithubKey = true;
-      };
-      tealdeer.enable = true;
-    };
-    utilities = {
-      fastfetch.enable = true;
-      nix-your-shell.enable = true;
-      starship.enable = true;
-    };
-    web = {
-      curl.enable = true;
-      wget.enable = true;
     };
     wms = {
       hyprland.enable = true;
@@ -100,8 +40,8 @@
   gtk = {
     enable = true;
     theme = {
-      name = "rose-pine";
-      package = pkgs.rose-pine-gtk-theme;
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
     };
     iconTheme = {
       name = "rose-pine";
@@ -125,7 +65,7 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-      gtk-theme = "rose-pine";
+      gtk-theme = "adw-gtk3-dark";
       icon-theme = "rose-pine";
       cursor-theme = "BreezeX-RosePine-Linux";
       cursor-size = 24;
@@ -147,9 +87,10 @@
   # :: HOME {
   home = {
     sessionVariables = {
-      GTK_THEME = "rose-pine";
+      GTK_THEME = "adw-gtk3-dark";
     };
     pointerCursor = {
+      enable = true;
       name = "BreezeX-RosePine-Linux";
       package = pkgs.rose-pine-cursor;
       size = 24;
