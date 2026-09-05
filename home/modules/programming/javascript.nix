39b9ea61deb5d087625faeabdb09fe7edc595cc9
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-09-05 12:35:04 +0200

    feat: merge into single home

--- home/modules/programming/javascript.nix
+++ home/modules/programming/javascript.nix
@@ -7,16 +7,18 @@
   options.me.programming.javascript.enable = lib.mkEnableOption "Enable Javascript Compiler";
 
   config = lib.mkIf config.me.programming.javascript.enable {
-    home.packages = with pkgs; [nodejs_latest];
+    home = {
+      packages = with pkgs; [nodejs_latest];
 
-    home.file.".npmrc".text = ''
-      prefix = ''${XDG_DATA_HOME}/npm
-      cache = ''${XDG_CACHE_HOME}/npm
-    '';
+      file.".npmrc".text = ''
+        prefix = ''${XDG_DATA_HOME}/npm
+        cache = ''${XDG_CACHE_HOME}/npm
+      '';
 
-    home.sessionPath = [
-      "$XDG_DATA_HOME/npm/bin"
-    ];
+      sessionPath = [
+        "$XDG_DATA_HOME/npm/bin"
+      ];
+    };
 
     programs.yarn.enable = true;
   };
